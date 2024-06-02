86ded38dcddfa84eb653cc030eda51a7a0ae6c0b
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-06-02 10:31:47 +0200

    FIXED MY META TAGS

--- pages/index.vue
+++ pages/index.vue
@@ -20,22 +20,14 @@ const valuesandcommitments =
 <head>
 <!-- HTML Meta Tags -->
 <title>AITS - Industrial Software Solutions And Digital Services Tailored To Your Business</title>
-<meta name="description" content="AITS transforms businesses like yours through innovative tech solutions tailored to your unique needs. Book your free consultation call to get started!">
+<meta name="description" content="AITS transforms businesses like yours through innovative tech solutions tailored to your unique needs. Book your free consultation call to get started!"/>
 
-<!-- Facebook Meta Tags -->
-<meta property="og:url" content="https://aitsmena-testtoproduction.netlify.app/">
-<meta property="og:type" content="website">
-<meta property="og:title" content="AITS - Industrial Software Solutions And Digital Services Tailored To Your Business">
-<meta property="og:description" content="AITS transforms businesses like yours through innovative tech solutions tailored to your unique needs. Book your free consultation call to get started!">
-<meta property="og:image" content="https://aitsmena-testtoproduction.netlify.app/aits-open-graph-bigger.jpg">
-
-<!-- Twitter Meta Tags -->
-<meta name="twitter:card" content="summary_large_image">
-<meta property="twitter:domain" content="aitsmena-testtoproduction.netlify.app">
-<meta property="twitter:url" content="https://aitsmena-testtoproduction.netlify.app/">
-<meta name="twitter:title" content="AITS - Industrial Software Solutions And Digital Services Tailored To Your Business">
-<meta name="twitter:description" content="AITS transforms businesses like yours through innovative tech solutions tailored to your unique needs. Book your free consultation call to get started!">
-<meta name="twitter:image" content="https://aitsmena-testtoproduction.netlify.app/aits-open-graph-bigger.jpg">
+<meta property="og:site_name" content="AITS">
+<meta property="og:title" content="AITS - Industrial Software Solutions And Digital Services Tailored To Your Business" />
+<meta property="og:description" content="AITS transforms businesses like yours through innovative tech solutions tailored to your unique needs. Book your free consultation call to get started!" />
+<meta property="og:image" itemprop="image" content="https://aitsmena-testtoproduction.netlify.app/aits-open-graph-bigger.jpg">
+<meta property="og:type" content="website" />
+<meta property="og:updated_time" content="1440432930" />
 
 <!-- Meta Tags Generated via https://www.opengraph.xyz -->    
 </head>
