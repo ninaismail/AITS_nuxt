1e9d12c45c7b671a8c355d84a2de143bb6835a6f
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-06-03 09:47:20 +0300

    cls fixes

--- components/Hero.vue
+++ components/Hero.vue
@@ -102,7 +102,7 @@ useHead({
  
       <!-- right layer -->
       <NuxtImg src="/images/hero/elements/righttop_small.webp" alt="AITS" width="720" height="749" 
-      class="min-h-screen w-screen absolute right-0 lg:top-1/4 lg:-translate-y-1/4 top-0 z-[-1]" center cover responsive loading="eager" />
+      class="absolute right-0 lg:top-1/4 top-0 z-[-1]" center cover responsive loading="eager" />
  
       <NuxtImg src="/images/hero/elements/rightmiddle_small.webp" alt="AITS" width="720" height="1151" center cover responsive loading="eager"
       v-motion  
